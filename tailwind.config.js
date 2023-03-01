6900384fd2dbf2c2b7820cf6288be8e92d09990a
Fahmi Ahsan <fahmiahsan00@gmail.com>
2023-03-01 12:37:57 +0700

    upload project

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        whatsapp: '#25D366',
+        instagram: '#E4405F',
+        facebook: '#1877F2',
+        youtube: '#FF0000',
+        discord: '#5865F2',
+        
+      },
+    },
+
+  },
+  plugins: [],
+}
